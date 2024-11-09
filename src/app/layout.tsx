d9f50312262c3049d5e1003c2d8cad93df70f6e5
samdahboss <samuelolawumi65@gmail.com>
2024-11-09 14:22:05 +0100

    feat: add favicon and update metadata for improved branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
   title: "CogNiche",
   description:
     "Cogniche is a powerful e-learning platform that provides online courses, skill development, and personalized learning experiences. Designed for learners at all levels, Cogniche offers a wide range of interactive courses, virtual classrooms, and expert-led tutorials across various fields. With flexible online education options, self-paced learning, and professional development tracks, Cogniche is the ideal online learning tool for students, professionals, and lifelong learners. Join Cogniche to access quality online education, connect with a global community, and unlock your potential from anywhere, at any time.",
+  icons: {
+    icon: "/brain-icon.png", // Set the favicon here
+  },
 };
 
 export default function RootLayout({
@@ -15,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link rel="icon" href="/brain-icon.png" />
+      </head>
       <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
