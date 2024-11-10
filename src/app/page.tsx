cd6884d33909e27a5965e202c369298c1f68909f
samdahboss <samuelolawumi65@gmail.com>
2024-11-10 21:32:40 +0100

    refactor: convert Home function to App component for improved structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import { Button } from "antd";
 import { UserButton } from "@clerk/nextjs";
 
-// const App: React.FC = () => (
-  
-// );
-
-export default async function Home(){
-<div className='flex flex-col gap-5 px-6 py-12'>
+const App: React.FC = () => (
+  <div className='flex flex-col gap-5 px-6 py-12'>
     <Button type='primary'>Primary Button</Button>
     <UserButton/>
     <Button type='text'>Text Button</Button>
     <Button type='link'>Link Button</Button>
   </div>
-}
+);
+
+export default App;
