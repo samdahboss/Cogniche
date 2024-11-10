1d7833478e5a6b08a430855e3c106c3c3fa0dcd7
samdahboss <samuelolawumi65@gmail.com>
2024-11-10 15:45:55 +0100

    COGNICHE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Button } from "antd";
+import { UserButton } from "@clerk/nextjs";
 
-const App: React.FC = () => (
-  <div className='flex flex-col gap-5 px-6 py-12'>
+// const App: React.FC = () => (
+  
+// );
+
+export default async function Home(){
+<div className='flex flex-col gap-5 px-6 py-12'>
     <Button type='primary'>Primary Button</Button>
-    <Button>Default Button</Button>
-    <Button type='dashed'>Dashed Button</Button>
+    <UserButton/>
     <Button type='text'>Text Button</Button>
     <Button type='link'>Link Button</Button>
   </div>
-);
-
-export default App;
+}
